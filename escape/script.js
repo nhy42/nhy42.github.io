69e0e89cb3f00c391800166a14a1b747c9ed0860
nhy42 <26034461+nhy42@users.noreply.github.com>
2025-09-22 23:06:53 +0200

    For ✨

---
+++ escape/script.js
@@ -0,0 +1,44 @@
+let timerInterval;
+let sanction=0;
+
+
+
+function startTimer() {
+  let timeRemaining = 3600;
+
+  updateDisplay(timeRemaining);
+
+  clearInterval(timerInterval);
+
+  timerInterval = setInterval(() => {
+    timeRemaining--;
+
+    if (sanction===1) {timeRemaining-=60; sanction=0}
+    updateDisplay(timeRemaining);
+
+    if (timeRemaining <= 0) {
+      clearInterval(timerInterval);
+
+      alert("Temps écoulé!");
+    }
+  }, 1000);
+}
+
+function sanctionTimer(){
+    sanction++
+}
+
+
+function updateDisplay(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  document.getElementById(
+    "countdownDisplay"
+  ).textContent = `${minutes}:${seconds.toString()
+    .padStart(2, "0")}`;
+}
+
+function stopTimer() {
+    clearInterval(timerInterval)
+}
